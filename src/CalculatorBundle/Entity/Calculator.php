87112b8a028054e99c82c3661f3107a7df5154db
TeodorDonchev <teodorgdo@gmail.com>
2017-08-02 01:16:27 +0300

    Initial commit

---
+++ src/CalculatorBundle/Entity/Calculator.php
@@ -0,0 +1,90 @@
+<?php
+namespace CalculatorBundle\Entity;
+
+class Calculator
+{
+ /**
+  * @var float
+  */
+    private  $leftOperand;
+
+    /**
+     * @var float
+     */
+    private  $rightOperand;
+
+    /**
+     * @var string
+     */
+    private $operator;
+
+
+     /**
+     * Get the left Operand
+     * @return float
+     */
+
+    public function getLeftOperand()
+    {
+      return $this->leftOperand;
+
+    }
+
+    /**
+     * Get the right Operand
+     * @return  float
+     */
+    public function getRightOperand()
+    {
+      return $this->rightOperand;
+    }
+
+    /**
+     * @param float $operand
+     * @return Calculator
+     */
+
+    public function setLeftOperand($operand)
+    {
+        $this->leftOperand = $operand;
+        return $this;
+    }
+    /**
+     * @param float $operand
+     * @return Calculator
+     */
+
+    public function setRightOperand($operand)
+    {
+        $this->rightOperand = $operand;
+        return $this;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getOperator()
+    {
+        return $this->operator;
+    }
+
+
+    /**
+     * @param string $operator
+     * @return Calculator
+     */
+    public function setOperator($operator)
+    {
+        $this->operator = $operator;
+        return $this;
+    }
+
+
+
+
+
+
+}
+
